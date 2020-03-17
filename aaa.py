4ccdf791f35cdff44421a1eb5d5c8e7ada67ffdd
Stefan Lapointe <stefanlapointe@Cindys-iMac.local>
2020-03-17 14:36:34 -0400

    aaa!

--- aaa.py
+++ aaa.py
@@ -1,1 +1,7 @@
 stres = int(input("how stres? (numeral): "))
+if stres < 10:
+    print(stres * "a")
+elif stres < 50:
+    print(stres * "A")
+else:
+    print(10 * "a" + 15 * "A" + (stres - 25) * "Ã")
